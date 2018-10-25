0d1ef1143bcc8096cdc9f644db94d627af073b1a
ChrisTimperley <christimperley@googlemail.com>
2018-10-25 12:59:12 -0400

    renamed input files

--- flex/test.sh
+++ flex/test.sh
@@ -6,14 +6,14 @@ inputs="${here_dir}/inputs"
 executable="${here_dir}/flex"
 
 case $test_id in
-  p1) "${executable}" < "${inputs}/t1" |& diff output.t1 - && exit 0;;
-  p2) "${executable}" < "${inputs}/t2" |& diff output.t2 - && exit 0;;
-  p3) "${executable}" < "${inputs}/t3" |& diff output.t3 - && exit 0;;
-  p4) "${executable}" < "${inputs}/t4" |& diff output.t4 - && exit 0;;
-  p5) "${executable}" < "${inputs}/t5" |& diff output.t5 - && exit 0;;
+  p1) "${executable}" < "${inputs}/p1" |& diff output.t1 - && exit 0;;
+  p2) "${executable}" < "${inputs}/p2" |& diff output.t2 - && exit 0;;
+  p3) "${executable}" < "${inputs}/p3" |& diff output.t3 - && exit 0;;
+  p4) "${executable}" < "${inputs}/p4" |& diff output.t4 - && exit 0;;
+  p5) "${executable}" < "${inputs}/p5" |& diff output.t5 - && exit 0;;
   n1)
     rm -rf core.*
-    "${executable}" < "${inputs}/t11"
+    "${executable}" < "${inputs}/n1"
     if [ ! -f core.* ] ; then exit 0; fi
 esac
 exit 1
