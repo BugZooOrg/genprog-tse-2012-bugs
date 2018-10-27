0e5366f359225e7fd9cd260f22cc2f286184b6fa
ChrisTimperley <christimperley@googlemail.com>
2018-10-27 12:45:19 -0400

    added inputs directory

--- uniq/test.sh
+++ uniq/test.sh
@@ -5,17 +5,11 @@ test_dir="${here_dir}/test"
 executable="${here_dir}/source/uniq"
 
 case $test_id in
-  p1)
-    "${executable}" < t1 &> /dev/null && exit 0;;
-  p2)
-    "${executable}" < t2 &> /dev/null && exit 0;;
-  p3)
-    "${executable}" < t4 &> /dev/null && exit 0;;
-  p4)
-    "${executable}" < tp1 &> /dev/null && exit 0;;
-  p5)
-    "${executable}" < tp2 &> /dev/null && exit 0;;
-  n1)
-    "${executable}" < t5 &> /dev/null && exit 0;;
+  p1) "${executable}" < p1 &> /dev/null && exit 0;;
+  p2) "${executable}" < p2 &> /dev/null && exit 0;;
+  p3) "${executable}" < p3 &> /dev/null && exit 0;;
+  p4) "${executable}" < p4 &> /dev/null && exit 0;;
+  p5) "${executable}" < p5 &> /dev/null && exit 0;;
+  n1) "${executable}" < n1 &> /dev/null && exit 0;;
 esac
 exit 1
