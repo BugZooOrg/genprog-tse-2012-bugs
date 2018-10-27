7706224d998d4b2fddcd6067972940476a76e976
ChrisTimperley <christimperley@googlemail.com>
2018-10-27 12:53:59 -0400

    fixed inputs in test script

--- uniq/test.sh
+++ uniq/test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 test_id=$1
 here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-test_dir="${here_dir}/test"
+inputs="${here_dir}/test"
 executable="${here_dir}/uniq"
 
 case $test_id in
-  p1) "${executable}" < p1 &> /dev/null && exit 0;;
-  p2) "${executable}" < p2 &> /dev/null && exit 0;;
-  p3) "${executable}" < p3 &> /dev/null && exit 0;;
-  p4) "${executable}" < p4 &> /dev/null && exit 0;;
-  p5) "${executable}" < p5 &> /dev/null && exit 0;;
-  n1) "${executable}" < n1 &> /dev/null && exit 0;;
+  p1) "${executable}" < "${inputs}/p1" &> /dev/null && exit 0;;
+  p2) "${executable}" < "${inputs}/p2" &> /dev/null && exit 0;;
+  p3) "${executable}" < "${inputs}/p3" &> /dev/null && exit 0;;
+  p4) "${executable}" < "${inputs}/p4" &> /dev/null && exit 0;;
+  p5) "${executable}" < "${inputs}/p5" &> /dev/null && exit 0;;
+  n1) "${executable}" < "${inputs}/n1" &> /dev/null && exit 0;;
 esac
 exit 1
