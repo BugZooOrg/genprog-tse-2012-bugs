1932a269bb6d6749ddf4ac09972fc16a6a13ac75
Joe Renzullo <renzullo@asu.edu>
2022-04-22 11:01:39 -0700

    added timeout to zune bug which indefinitely hangs

--- zune/test.sh
+++ zune/test.sh
@@ -6,6 +6,6 @@ executable="/experiment/source/zune"
 
 case $test_id in
   p1|p2|p3|p4|p5|p6|p7|p8|p9|p10|p11|p12|p13|p14|p15|p16|p17|p18|n1|n2|n3|n4)
-    $executable $(cat $test_dir/$test_id) |& diff $test_dir/output.$test_id - && exit 0;;
+    timeout 5 $executable $(cat $test_dir/$test_id) |& diff $test_dir/output.$test_id - && exit 0;;
 esac
 exit 1
